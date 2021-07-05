6fe39c6ab411495e52687dc5d80688dcc20ffa94
Barabanov Ivan <barabanovivan@bk.ru>
2021-07-05 22:49:15 +0400

    Добавлено немножко дизайна

--- ClientApp/src/App.vue
+++ ClientApp/src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <div id="nav"><router-link to="/">Home</router-link></div>
   <router-view />
 </template>
 
 <style>
+body {
+  background-color:#2c3e50;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- ClientApp/src/components/HelloWorld.vue
+++ ClientApp/src/components/HelloWorld.vue
@@ -1,48 +1,47 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
-        <h1>{{ test }}</h1>
+  <div v-if="loaded">
+    <div class="card" v-if="works.length">
+        <div v-for="work in works" :key="work.Id">
+          <span>{{ work.Name }}</span>
+        </div>
     </div>
+    <h1 v-else>Oops, there is nothing...</h1>
+  </div>
 </template>
 
 <script>
-    import axios from 'axios'
+import axios from "axios";
 
 export default {
-    name: "HelloWorld",
-    data() {
-        return {
-            test: 'test'
-        }
-    },
-    props: {
-        msg: String,
-    },
-    mounted() {
-        axios.get('/Work')
-            .then((response) => {
-                this.test = response.data;
-            })
-            .catch(function (error) {
-                this.test = error;
-            });
-    }
+  name: "HelloWorld",
+  data() {
+    return {
+      loaded: false,
+      works: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("/Work")
+      .then((response) => {
+        this.works = response.data;
+      })
+      .catch((error) => {
+        alert("OMG! Something goes wrong!");
+        console.error(error);
+      })
+      .finally(() => (this.loaded = true));
+  },
 };
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.card {
+  width: 80%;
+  background-color: white;
+  padding: 1rem 5rem;
+  border-radius: 1.2rem;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
